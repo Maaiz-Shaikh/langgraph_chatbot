b48fbafab4c18f1d44a5688ced19da34b8dbc5e2
Maaiz Shaikh <maaizshaikhofficial@gmail.com>
2026-05-28 09:06:06 +0530

    integrated sqlite database for permanent storage

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -3,9 +3,10 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.graph.message import add_messages
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage
-from langgraph.checkpoint.memory import MemorySaver 
+from langgraph.checkpoint.sqlite import SqliteSaver 
 from dotenv import load_dotenv
 import os
+import sqlite3
 
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
@@ -19,7 +20,15 @@ def chat_node(state: ChatState) -> ChatState:
     response = llm.invoke(messages)
     return {'messages': [response]}
 
-checkpointer = MemorySaver()
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config["configurable"]["thread_id"])
+    
+    return list(all_threads)
+
+conn = sqlite3.connect('chatbot.db', check_same_thread=False)
+checkpointer = SqliteSaver(conn=conn)
 
 graph = StateGraph(ChatState)
 graph.add_node('chat_node', chat_node)
